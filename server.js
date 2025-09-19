6507c9c6424ce7e5e80b29f8d18d63706ceab28a
SoponSaenkaew <soponsaenkaew@gmail.com>
2025-09-19 20:42:09 +0700

    test1

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.use(router);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => console.log(`Listening on ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Listening on ${PORT} -> http://localhost:3000`));
